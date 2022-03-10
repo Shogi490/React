7ded5ec1f3070a24f2e055d501ecb1758ad715b7
Ethan Pulido <39225221+epulido610@users.noreply.github.com>
2022-03-10 10:28:51 -0800

    video in backround of homePage

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -7,17 +7,19 @@ function Home() {
   const history = useNavigate();
   return (
   
-	<>
-		<h1> Welcome to Shogi Online </h1>
-    <div className="landing-container">
-      <div className="block--display">
-      <button className="home--button" onClick={() => {history('Play')}}>Play Shogi</button>
-      <button className="home--button" onClick={() => {history('Learn')}}>Learn Shogi</button>
-      <button className="home--button" onClick={() => {history('sign-up')}}>Sign up</button>
-      </div>
-    </div>
+    <div className="section">
+        <h1> Shogi </h1>
+        <div class = "video-container">
+          <div class = "color-overlay"> 
+        </div>
+        <iframe src="https://www.youtube.com/embed/ga_ivdZ7es0?vq=hd1080&autoplay=1&loop=1&modestbranding=1&showinfo=0&rel=0&cc_load_policy=1&iv_load_policy=3&theme=light&fs=0&controls=0&disablekb=1&mute=1&loop=1" width="1920" height="1080" frameborder="0"></iframe>
+        </div>
 
-   </>
+          <button className="home--button" onClick={() => {history('Play')}}>Play Shogi</button>
+          <button className="home--button" onClick={() => {history('Learn')}}>Learn Shogi</button>
+          <button className="home--button" onClick={() => {history('sign-up')}}>Sign up</button>
+
+      </div>
   );
 }
 
