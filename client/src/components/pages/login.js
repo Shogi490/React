11ec7948ded8e0c2f034663529e560052f4204eb
JRush <jrjakerush@gmail.com>
2022-03-16 18:31:44 -0700

    reset

--- client/src/components/pages/login.js
+++ client/src/components/pages/login.js
@@ -1,53 +1,15 @@
 import React, { useState } from 'react'
 import axios from "axios";
-import { AuthContext } from '../../App';
 import "./Signup.css"
 
 
 const Login = () => {
-  const { dispatch } = React.useContext(AuthContext);
-  const initialState = {
-    username: "",
-    password: "",
-    errorMessage: null
-  };
-
-  const [data, setData] = React.useState(initialState);
-
-  const handleInputChange = event => {
-    setData({
-      ...data,
-      [event.target.name]: event.target.value
-    });
-  };
-
-
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState(""); 
   const handleSubmit = event => {
     event.preventDefault();
-    setData({
-      ...data,
-      errorMessage: null
-    });
-    axios.post("http://localhost:5000/login", JSON.stringify({username: data.username, password: data.password})).then(res => {
-      if (res.ok) {
-        return res.json();
-      }
-      throw res;
-    })
-    .then(resJson => {
-      dispatch({
-          type: "LOGIN",
-          payload: resJson
-      })
-    })
-    .catch(error => {
-      setData({
-        ...data,
-        isSubmitting: false,
-        errorMessage: error.message || error.statusText
-      });
-    });
-};
+    axios.post("http://localhost:5000/login", {username, password}).then(res => {console.log(res); console.log(res.data);});
+  }
   return(
     <>
     <div className="login-wrapper">
@@ -55,18 +17,15 @@ const Login = () => {
       <form className="signup-form" onSubmit={handleSubmit}>
         <label>
           <p>Username *</p>
-          <input type="text" placeholder="Username" name="username" value={data.username} onChange={e => onChange(handleInputChange)} required />
+          <input type="text" placeholder="Username" name="username" value={username} onChange={e => setUsername(e.target.value)} required />
         </label>
         <label>
           <p>Password *</p>
-          <input type="password" placeholder="Password" name="password" value={data.password} onChange={e => onChange(handleInputChange)} required />
+          <input type="password" placeholder="Password" name="password" value={password} onChange={e => setPassword(e.target.value)} required />
         </label>
         <div>
           <button className="submitbutton" type="submit">Login</button>
         </div>
-          {data.errorMessage && (
-            <span className="form-error">{data.errorMessage}</span>
-          )}
       </form>
       </div>
     </>
