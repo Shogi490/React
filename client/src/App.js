02141d8f27512b954696f7a7dc3399d498292440
JRush <jrjakerush@gmail.com>
2022-03-16 18:29:52 -0700

    test

--- client/src/App.js
+++ client/src/App.js
@@ -10,11 +10,57 @@ import PageNotFound from './components/pages/PageNotFound';
 import Login from './components/pages/login';
 import UserProfile from "./components/pages/UserProfile.js"
 
+export const AuthContext = React.createContext();
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  token: null
+};
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case "LOGIN":
+      localStorage.setItem("user", JSON.stringify(action.payload.user));
+      localStorage.setItem("token", JSON.stringify(action.payload.token));
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+        token: action.payload.token
+      };
+    case "LOGOUT":
+      localStorage.clear();
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null
+      }
+    default:
+      return state;
+  }
+}
 
 
-function App() {  
+function App() {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  React.useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user') || null)
+    const token = JSON.parse(localStorage.getItem('token') || null)
+    if(user && token){
+      dispatch({
+        type: 'LOGIN',
+        payload: {
+          user,
+          token
+        }
+      })
+    }
+  }, [])
+
   return (
     <>
+      <AuthContext.Provider value={{state, dispatch}}>
       <Router>
         <Navbar/>
         <Routes>
@@ -31,7 +77,7 @@ function App() {
           <Route path='/user/:id' element={<UserProfile/>}/>
         </Routes>
       </Router>
-
+      </AuthContext.Provider>
     </>
   );
 }
