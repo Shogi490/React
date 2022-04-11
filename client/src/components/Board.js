070fa0c8f7e9e8a30e38ce5004e8b60f69d86321
birdsnbears <louischu15@gmail.com>
2022-04-11 10:35:53 -0700

    Added Unity Project and displayed it in Board Component
    The board component now has a small version of the game running in it.

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import "./Board.css"
+import Unity, { UnityContext } from "react-unity-webgl";
+
+const unityContext = new UnityContext({
+    loaderUrl: "build/Build/build.loader.js",
+    dataUrl: "build/Build/build.data",
+    frameworkUrl: "build/Build/build.framework.js",
+    codeUrl: "build/Build/build.wasm",
+});
 
 function Board() {
     return (
         <>
         <div className="gameWrapper">
-            <div className="shogiBoard"></div>
+            <div className="shogiBoard">
+                <Unity unityContext={unityContext} />
+            </div>
             <div className="moveTracker"></div>
         </div>
         </>
--- client/src/components/pages/Play.js
+++ client/src/components/pages/Play.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Board from '../Board';
-//import unity-react?
 
 function Play () {
 
