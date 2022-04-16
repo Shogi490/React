986cbd8aea6695a433ca0ea83183db98f8704c17
birdsnbears <louischu15@gmail.com>
2022-04-16 16:51:00 -0700

    added temp functions to Board.js
    these are template functions from the npm documentation that we might be able to use when initializing the game.

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Board.css"
 import Unity, { UnityContext } from "react-unity-webgl";
 
@@ -9,15 +9,46 @@ const unityContext = new UnityContext({
     codeUrl: "build/Build/build.wasm",
 });
 
-function Board() {
+function Board({ game }) {
+
+    const [progression, setProgression] = useState(0);
+    // const [moves, setMoves] = useState(game.moveHistory);
+
+    useEffect(function () {
+        // the board component can only exist if a game is currently ongoing.
+        // we should expect some kind of gameID and find the movehistory of that game.
+        // mayhaps whomever is creating the board should send that to us.
+    })
+
+    useEffect(function () {
+        unityContext.on("progress", function (progression) {
+            // even if progression is 100%, this does NOT mean we're done initializing. We still need to send Unity some initilization values.
+            setProgression(progression);
+        });
+    }, []);
+
+    useEffect(function () {
+        unityContext.on("loaded", function () {
+            // before this, hide Unity or show some kind of loading thing.
+            console.log("Unity has finished loading!");
+            // Initialize Unity.
+            // display Unity.
+        });
+    }, []);
+
     return (
         <>
-        <div className="gameWrapper">
-            <div className="shogiBoard">
-                <Unity unityContext={unityContext} />
+            <div className="gameWrapper">
+                <div className="shogiBoard">
+                    <Unity
+                        unityContext={unityContext}
+                        style={{
+                            width: "100%"
+                        }}
+                    />
+                </div>
+                <div className="moveTracker"></div>
             </div>
-            <div className="moveTracker"></div>
-        </div>
         </>
     )
 }
