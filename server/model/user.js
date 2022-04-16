dc135b20f8d2e3739d239e8a2bc0137e79c4d29b
birdsnbears <louischu15@gmail.com>
2022-04-16 16:47:30 -0700

    added potential gameSchema

--- server/model/user.js
+++ server/model/user.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
@@ -5,7 +6,23 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var UserSchema = new Schema({   
     email: {type: String, required:true, unique:true},
     username : {type: String, unique: true, required:true},
+    games: [ObjectId],
 });
+
+var gameSchema = new Schema({
+    isComputerGame: Boolean,
+    creatorID: {type: ObjectId, required: true, immutable: true},
+    opponentID: {type: UserSchema, required: false, immutable: true}, // if isComputerGame is True, this field shouldn't exist.
+    creatorIsBlack : Boolean,
+    moveHistory : [String],
+    currentSFEN: String,
+    timeMade : Date,
+    timeControl : String,
+    minutesPerSide : Number,
+    byoyomiInSeconds : Number,
+    correspondence: Number,
+    dateSinceLastCorrespondence: Date,
+})
   
 UserSchema.plugin(passportLocalMongoose);
   
