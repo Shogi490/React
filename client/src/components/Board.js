5fa1a41cf3d8e058224891f115fea9287b1c3c3f
birdsnbears <louischu15@gmail.com>
2022-04-24 17:33:31 -0700

    Added Expected Event Listeners

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import "./Board.css"
 import Unity, { UnityContext } from "react-unity-webgl";
+import Attacks from "shogiops/attacks"
 import { makeSfen } from "shogiops/sfen";
 import { parseUsi } from 'shogiops/util';
 import { Shogi } from 'shogiops/shogi';
@@ -14,48 +15,62 @@ const unityContext = new UnityContext({
 
 function Board({ gameInitSettings }) {
 
-    const [progression, setProgression] = useState(0);
+    const [isLoaded, setIsLoaded] = useState(false);
     const [game, setGame] = useState(Shogi.default());
     // const [moves, setMoves] = useState(game.moveHistory);
 
+    // Obtain game info? Rush was supposed to help w/ this.
     useEffect(function () {
-        // if game has a different starting sfen/moveset. This is where we would overwrite game.
-        const move = parseUsi('7g7f');
-        game.play(move);
-        console.log(makeSfen(game.toSetup()));
-    }, [])
-
-    useEffect(function () {
+        // setGameRecord(gameRecord); or some shit
         // the board component can only exist if a game is currently ongoing.
         // we should expect some kind of gameID and find the movehistory of that game.
         // mayhaps whomever is creating the board should send that to us.
-    })
+    }, [])
 
+    // Unity Event Responses
     useEffect(function () {
-        unityContext.on("progress", function (progression) {
-            // even if progression is 100%, this does NOT mean we're done initializing. We still need to send Unity some initilization values.
-            setProgression(progression);
-        });
-        unityContext.on("Move", function (usiMove) {
-            // const move = parseUsi(usiMove);
-            // game.play(move);
-            // sendMoveToEnemy?
-        })
-    }, []);
-
-    useEffect(function () {
+        // When Unity finishes loading. Initialize Shogiops.
         unityContext.on("loaded", function () {
             // before this, hide Unity or show some kind of loading thing.
             console.log("Unity has finished loading!");
             // Initialize Unity.
+            // setGame(Shogi.default);
+            // for(let move of dbGameRecord.moves){
+            //     game.move(parseUsi(move));
+            // }
             // display Unity.
+            setIsLoaded(true);
+        });
+        // When player clicked on their piece and wants to know where it can move
+        unityContext.on("WantsToMove", function ( square ) {
+            let arr = SquareSetToArray(game.dests(square));
+            DisplayArray(arr);
+            unityContext.send("GameController", "HighlightMoves", arr);
+        });
+        // When player clicked on a valid destination for the piece they wanted to MOVE
+        unityContext.on("Move", function (usiMove) {
+            const move = parseUsi(usiMove);
+            game.play(move);
+            // sendMoveToEnemy?
+        });
+        // When player clicked in a valid destination for a piece they wanted to DROP
+        unityContext.on("Drop", function (usiMove) {
+            const move = parseUsi(usiMove);
+            game.play(move);
+            // sendMoveToEnemy?
         });
     }, []);
+
+    function HighlightDropDests (roleName){
+        let arr = SquareSetToArray(game.dropDests(roleName));
+        DisplayArray(arr);
+        unityContext.send("GameController", "HighlightDrops", arr);
+    }
 
     return (
         <>
             <div className="gameWrapper">
-                <div className="shogiBoard">
+                <div className="shogiBoard" style={{ visibility : isLoaded ? "visible" : "hidden"}}>
                     <Unity
                         unityContext={unityContext}
                         style={{
@@ -67,6 +82,22 @@ function Board({ gameInitSettings }) {
             </div>
         </>
     )
+
+    function SquareSetToArray ( squareSet ) {
+        let arr = new Array();
+        let i = 0;
+        for(let square of squareSet){
+            arr[i] = square;
+            i++;
+        }
+        return arr;
+    }
+
+    function DisplayArray ( arr ){
+        for(let victim of arr){
+            console.log(victim);
+        }
+    }
 }
 
 export default Board;
